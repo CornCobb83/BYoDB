a29d9754ca240914d70257de1888e18ae869128b
CornCobb83 <143015909+CornCobb83@users.noreply.github.com>
2026-02-05 19:42:14 -0800

    Sample files to display the functionality

---
+++ samples/325populate.sql
@@ -0,0 +1,294 @@
+-- Inserts for MANAGER
+Insert into manager(MANAGER_ID, yrs_exp) values (114, 9);
+Insert into manager(MANAGER_ID, yrs_exp) values (115, 11);
+Insert into manager(MANAGER_ID, yrs_exp) values (116, 17);
+Insert into manager(MANAGER_ID, yrs_exp) values (117, 32);
+Insert into manager(MANAGER_ID, yrs_exp) values (118, 21);
+Insert into manager(MANAGER_ID, yrs_exp) values (119, 12);
+Insert into manager(MANAGER_ID, yrs_exp) values (120, 2);
+Insert into manager(MANAGER_ID, yrs_exp) values (121, 13);
+Insert into manager(MANAGER_ID, yrs_exp) values (122, 15);
+Insert into manager(MANAGER_ID, yrs_exp) values (123, 8);
+Insert into manager(MANAGER_ID, yrs_exp) values (124, 37);
+Insert into manager(MANAGER_ID, yrs_exp) values (125, 6);
+
+-- Inserts for EMPLOYEE
+Insert into employee values (5121, 'Jack', 'Nudd', 14, 60000);
+Insert into employee values (5120, 'Norman', 'Jansen', 5, 30000);
+Insert into employee values (5119, 'Jocelyn', 'Crants', 21, 70000);
+Insert into employee values (5118, 'Aiden', 'Rogers', 5, 30000);
+Insert into employee values (5109, 'Cecelia', 'Garcia', 5, 30000);
+Insert into employee values (5117, 'Manny', 'Xavier', 21, 80000);
+Insert into employee values (5116, 'Penn', 'Hunt', 2, 30000);
+Insert into employee values (5115, 'Lue', 'Ardon', 7, 35000);
+Insert into employee values (5114, 'Duke', 'Robbinginton', 7, 37000);
+Insert into employee values (5113, 'Sam', 'Palt', 10, 40000);
+Insert into employee values (5112, 'Donny', 'Willis', 10, 42000);
+Insert into employee values (5111, 'Sam', 'Cook', 10, 42500);
+Insert into employee values (5110, 'Luke', 'Delimitri', 10, 42500);
+
+-- Inserts for DEPARTMENT
+Insert into department(DEPT_ID, dept_name) values (553, 'Management');
+Insert into department(DEPT_ID, dept_name) values (552, 'Marketing');
+Insert into department(DEPT_ID, dept_name) values (551, 'Purchasing');
+insert into department(DEPT_ID, dept_name) values (550, 'Customer Care');
+insert into department(DEPT_ID, dept_name) values (549, 'Administration');
+insert into department(DEPT_ID, dept_name) values (548, 'Research');
+insert into department(DEPT_ID, dept_name) values (547, 'Shipping');
+insert into department(DEPT_ID, dept_name) values (546, 'Packaging');
+insert into department(DEPT_ID, dept_name) values (545, 'Service');
+insert into department(DEPT_ID, dept_name) values (544, 'Finance');
+insert into department(DEPT_ID, dept_name) values (543, 'Legal');
+insert into department(DEPT_ID, dept_name) values (542, 'Information');
+insert into department(DEPT_ID, dept_name) values (541, 'Accounting');
+
+-- Inserts for ITEM
+Insert into item values (00003, 'physical', 'punk rock', 5.99, '18-JAN-1970');
+Insert into item values (00004, 'physical', 'blues', 3.99, '01-NOV-1989');
+Insert into item values (00005, 'physical', 'pop', 4.99, '23-DEC-1988');
+Insert into item values (00006, 'physical', 'classical', 5.99, '25-JUN-1961');
+Insert into item values (00007, 'physical', 'blues', 3.99, '09-JUL-1952');
+Insert into item values (00008, 'physical', 'jazz', 8.99, '05-NOV-1934');
+Insert into item values (00009, 'physical', 'alternative', 3.99, '03-FEB-1989');
+Insert into item values (00010, 'physical', 'jazz', 4.99, '13-MAY-1962');
+Insert into item values (00011, 'physical', 'pop', 5.99, '15-FEB-1988');
+Insert into item values (00012, 'physical', 'rock', 4.99, '16-MAR-1981');
+Insert into item values (00013, 'physical', 'heavy metal', 3.99, '11-JAN-1973');
+Insert into item values (00014, 'physical', 'funk', 4.99, '10-DEC-1963');
+Insert into item values (00015, 'physical', 'blues', 5.99, '08-JAN-1981');
+Insert into item values (00016, 'physical', 'classical', 9.99, '09-JAN-1985');
+Insert into item values (00017, 'physical', 'rock', 3.99, '11-SEP-1942');
+Insert into item values (00018, 'physical', 'blues', 4.99, '20-OCT-1932');
+Insert into item values (00019, 'physical', 'funk', 3.99, '19-APR-1978');
+Insert into item values (00020, 'physical', 'pop', 2.99, '16-NOV-1977');
+Insert into item values (00021, 'physical', 'country', 4.99, '15-SEP-1962');
+Insert into item values (00022, 'physical', 'jazz', 5.99, '13-DEC-1957');
+Insert into item values (00023, 'physical', 'rock', 6.99, '22-MAY-1982');
+Insert into item values (00024, 'physical', 'alternative', 4.99, '05-JUN-1982');
+Insert into item values (00025, 'physical', 'jazz', 3.99, '28-JUL-1962');
+Insert into item values (00026, 'physical', 'classical', 4.99, '03-JUN-1978');
+Insert into item values (00027, 'physical', 'heavy metal', 3.99, '28-JUL-1976');
+Insert into item values (00028, 'physical', 'jazz', 3.99, '28-JUL-1945');
+Insert into item values (00029, 'physical', 'blues', 2.99, '28-JUL-1986');
+Insert into item values (00030, 'physical', 'country', 4.99, '28-JUL-1984');
+Insert into item values (00031, 'physical', 'funk', 3.99, '28-JUL-1982');
+Insert into item values (00032, 'physical', 'pop', 2.99, '28-JUL-1975');
+Insert into item values (00033, 'physical', 'alternative', 4.99, '28-JUL-1952');
+Insert into item values (00098, 'digital', 'jazz', 1.99, '15-JUN-1943');
+Insert into item values (00099, 'digital', 'punk', 2.99, '02-DEC-1985');
+Insert into item values (00100, 'digital', 'blues', 2.99, '23-SEP-1941');
+Insert into item values (00101, 'digital', 'pop', 3.99, '21-JUL-1873');
+Insert into item values (00102, 'digital', 'heavy metal', 2.99, '21-JUL-1975');
+Insert into item values (00103, 'digital', 'rock', 4.99, '21-JUL-1929');
+Insert into item values (00104, 'digital', 'classical', 3.99, '21-JUL-1873');
+
+-- Inserts for DIGITAL_ITEM
+Insert into digital_item values (00099, 'aac', 3);
+Insert into digital_item values (00100, 'aac', 3.5);
+Insert into digital_item values (000101, 'mp3', 3);
+Insert into digital_item values (000102, 'mp3', 4.5);
+Insert into digital_item values (000103, 'aac', 3);
+Insert into digital_item values (000104, 'mp3', 5.0);
+
+-- Inserts for PHYSICAL_ITEM
+Insert into physical_item values (00003, 'record', 10);
+Insert into physical_item values (00004, 'record', 7);
+Insert into physical_item values (00005, 'record', 12);
+Insert into physical_item values (00006, 'cd', 4.7);
+Insert into physical_item values (00007, 'cd', 3.13);
+Insert into physical_item values (00008, 'cd', 4.7);
+Insert into physical_item values (00009, 'cassette', 4.25);
+Insert into physical_item values (00010, 'cassette', 4.25);
+Insert into physical_item values (00011, 'record', 12);
+Insert into physical_item values (00012, 'record', 12);
+Insert into physical_item values (00013, 'record', 7);
+Insert into physical_item values (00014, 'record', 10);
+Insert into physical_item values (00015, 'record', 12);
+Insert into physical_item values (00016, 'cd', 4.7);
+Insert into physical_item values (00017, 'cd', 4.7);
+Insert into physical_item values (00018, 'cd', 4.7);
+Insert into physical_item values (00019, 'cassette', 4.25);
+Insert into physical_item values (00020, 'cassette', 4.25);
+Insert into physical_item values (00021, 'record', 12);
+Insert into physical_item values (00022, 'record', 10);
+Insert into physical_item values (00023, 'record', 10);
+Insert into physical_item values (00024, 'record', 12);
+Insert into physical_item values (00025, 'cd', 4.7);
+Insert into physical_item values (00026, 'record', 4.7);
+Insert into physical_item values (00027, 'cassette', 4.25);
+Insert into physical_item values (00028, 'cd', 3.13);
+Insert into physical_item values (00029, 'record', 12);
+Insert into physical_item values (00030, 'cassette', 4.25);
+Insert into physical_item values (00031, 'record', 7);
+Insert into physical_item values (00032, 'cd', 4.7);
+Insert into physical_item values (00033, 'record', 10);
+
+-- Inserts for MEMBERS
+insert into members values (2123, 3257654892, 'jazz', '17-Jul-1997');
+insert into members values (2124, 5306267492, 'country', '2-Aug-2000');
+insert into members values (2125, 7483456968, 'pop', '3-Dec-2004');
+insert into members values (2126, 3256748213, 'rock', '28-Feb-2000');
+insert into members values (2127, 8055555555, 'jazz', '17-Mar-1973');
+insert into members values (2128, 3247688293, 'disco', '14-Nov-1965');
+insert into members values (2129, 4356769825, 'blues', '1-Jan-2001');
+insert into members values (2130, 2817453361, 'country', '8-Mar-1989');
+insert into members values (2131, 7584567737, 'blues', '17-Jul-1973');
+insert into members values (2132, 2817435372, 'classical', '3-Feb-1963');
+insert into members values (2133, 3427683490, 'disco', '17-Jul-2001');
+insert into members values (2134, 7893451234, 'classical', '18-Aug-1977');
+insert into members values (2135, 1234567890, 'blues', '10-Jun-1991');
+insert into members values (2136, 2817453264, 'country', '17-Jul-1998');
+
+-- Inserts for EMPLOYEE_MANAGER
+Insert into employee_manager values (5121, 113);
+Insert into employee_manager values (5120, 113);
+Insert into employee_manager values (5119, 114);
+Insert into employee_manager values (5118, 115);
+Insert into employee_manager values (5109, 115);
+Insert into employee_manager values (5117, 116);
+Insert into employee_manager values (5116, 117);
+Insert into employee_manager values (5115, 118);
+Insert into employee_manager values (5114, 119);
+Insert into employee_manager values (5113, 120);
+Insert into employee_manager values (5111, 122);
+Insert into employee_manager values (5110, 123);
+
+-- Inserts for EMPLOYEE_DEPT
+Insert into employee_dept values (5122, 555);
+Insert into employee_dept values (5121, 553);
+Insert into employee_dept values (5120, 552);
+Insert into employee_dept values (5119, 552);
+Insert into employee_dept values (5119, 548);
+Insert into employee_dept values (5118, 551);
+Insert into employee_dept values (5109, 551);
+Insert into employee_dept values (5117, 550);
+Insert into employee_dept values (5116, 549);
+Insert into employee_dept values (5111, 544);
+Insert into employee_dept values (5115, 548);
+Insert into employee_dept values (5114, 547);
+Insert into employee_dept values (5113, 546);
+Insert into employee_dept values (5112, 545);
+Insert into employee_dept values (5116, 550);
+Insert into employee_dept values (5111, 543);
+Insert into employee_dept values (5110, 543);
+
+-- Inserts for MANAGER_DEPT
+Insert into manager_dept values (113, 553);
+Insert into manager_dept values (114, 552);
+Insert into manager_dept values (115, 551);
+Insert into manager_dept values (116, 550);
+Insert into manager_dept values (117, 549);
+Insert into manager_dept values (118, 548);
+Insert into manager_dept values (119, 547);
+Insert into manager_dept values (120, 546);
+Insert into manager_dept values (121, 545);
+Insert into manager_dept values (122, 544);
+Insert into manager_dept values (123, 543);
+Insert into manager_dept values (124, 542);
+Insert into manager_dept values (125, 541);
+Insert into manager_dept values (117, 548);
+Insert into manager_dept values (112, 553);
+
+-- Inserts for CUSTOMER
+Insert into customer values (0003, 2123, 8077322345);
+Insert into customer values (0004, 2124, 5301940023);
+Insert into customer values (0005, 2125, 7071236574);
+Insert into customer values (0006, 2126, 4512765577);
+Insert into customer values (0007, 2127, 9417863542);
+Insert into customer values (0008, 2128, 9094785334);
+Insert into customer values (0009, 2129, 5307733256);
+Insert into customer values (0010, 2130, 3308857654);
+Insert into customer values (0011, 2131, 2108753910);
+Insert into customer values (0012, 2132, 4508769900);
+Insert into customer values (0013, 2133, 3762874556);
+Insert into customer values (0014, 2134, 3456273344);
+Insert into customer values (0015, 2135, 7073452165);
+
+-- Inserts for RENTAL
+Insert into rental values (911, '14-FEB-2024', '01-MAR-2024', 00023, 2126);
+Insert into rental values (912, '20-FEB-2024', '07-MAR-2024', 00001, 2122);
+Insert into rental values (913, '01-MAR-2024', '14-MAR-2024', 00002, 2128);
+Insert into rental values (914, '01-MAY-2024', '14-MAY-2024', 00001, 2123);
+Insert into rental values (915, '04-JUN-2024', '18-JUN-2024', 00013, 2129);
+Insert into rental values (916, '07-JUL-2024', '21-JUL-2024', 00005, 2127);
+Insert into rental values (917, '20-JUL-2024', '03-AUG-2024', 00006, 2130);
+Insert into rental values (918, '03-AUG-2024', '17-AUG-2024', 00007, 2131);
+Insert into rental values (919, '7-DEC-2024', '25-JAN-2025', 00021, 2124);
+Insert into rental values (920, '13-DEC-2024', '07-JAN-2025', 00006, 2134);
+Insert into rental values (921, '05-SEP-2024', '19-SEP-2024', 00004, 2136);
+Insert into rental values (922, '05-SEP-2024', '19-SEP-2024', 00001, 2133);
+Insert into rental values (923, '04-OCT-2024', '18-OCT-2024', 00018, 2135);
+Insert into rental values (924, '05-OCT-2024', '19-OCT-2024', 00005, 2132);
+
+-- Inserts for MEMBER_DATE
+Insert into member_date values (2122,  '01-SEP-2012');
+Insert into member_date values (2123,  '12-OCT-2017');
+Insert into member_date values (2124,  '13-OCT-2016');
+Insert into member_date values (2125,  '20-OCT-2021');
+Insert into member_date values (2126,  '25-AUG-2023');
+Insert into member_date values (2127,  '23-JUN-2024');
+Insert into member_date values (2128,  '17-JUL-2024');
+Insert into member_date values (2129,  '15-MAR-2022');
+Insert into member_date values (2130,  '16-MAY-2023');
+Insert into member_date values (2131,  '04-APR-2024');
+Insert into member_date values (2132,  '01-APR-2024');
+Insert into member_date values (2133,  '02-APR-2024');
+Insert into member_date values (2134,  '02-APR-2024');
+Insert into member_date values (2135,  '03-APR-2024');
+Insert into member_date values (2136,  '04-APR-2024');
+
+-- Inserts for MEMBER_EMAILS
+Insert into member_emails(MEMBER_ID, EMAIL) values (2123, 'juliab@gmail.com');
+Insert into member_emails values (2124, 'jamesgt@gmail.com');
+Insert into member_emails values (2125, 'johntt@gmail.com');
+Insert into member_emails values (2126, 'dylano@gmail.com');
+Insert into member_emails values (2127, 'ivyrd@gmail.com');
+Insert into member_emails values (2128, 'drcornelia@gmail.com');
+Insert into member_emails values (2129, 'vampstacy@hotmail.com');
+Insert into member_emails values (2130, 'timb@yahoo.com');
+Insert into member_emails values (2131, 'ginaUT@umb.edu');
+Insert into member_emails values (2132, 'thorn21@gmail.com');
+Insert into member_emails values (2133, 'aaa56@gmail.com');
+Insert into member_emails values (2134, 'CIYD6478@gmail.com');
+Insert into member_emails values (2135, 'holland34@gmail.com');
+Insert into member_emails values (2136, '56fish@gmail.com');
+
+-- Inserts for TRANSACTIONS
+INSERT INTO TRANSACTIONS (TRANSACTION_ID, ITEM_ID, CUSTOMER_ID, TRANSACTION_DATE, EMPL_ID) VALUES (01236, 00014, 0003, '21-JUN-2024', 5121);
+INSERT INTO TRANSACTIONS (TRANSACTION_ID, ITEM_ID, CUSTOMER_ID, TRANSACTION_DATE, EMPL_ID) VALUES (01237, 00015, 0004, '23-Nov-2024', 5120);
+insert into transactions (TRANSACTION_ID, item_id, customer_id, transaction_date) values (01238, 00016, 0005, '28-SEP-2024');
+insert into transactions (TRANSACTION_ID, item_id, customer_id, transaction_date) values (01239, 00017, 0006, '19-Nov-2024');
+insert into transactions (TRANSACTION_ID, item_id, customer_id, transaction_date) values (01240, 00018, 0007, '30-Nov-2024');
+insert into transactions (TRANSACTION_ID, item_id, customer_id, transaction_date) values (01241, 00019, 0008, '14-Dec-2024');
+insert into transactions (TRANSACTION_ID, item_id, customer_id, transaction_date) values (01242, 00020, 0009, '23-Nov-2024');
+insert into transactions (TRANSACTION_ID, item_id, customer_id, transaction_date) values (01243, 00020, 0010, '29-Oct-2024');
+insert into transactions (TRANSACTION_ID, item_id, customer_id, transaction_date) values (01244, 00022, 0011, '01-Dec-2024');
+insert into transactions (TRANSACTION_ID, item_id, customer_id, transaction_date) values (01245, 00023, 0002, '19-Aug-2024');
+insert into transactions (TRANSACTION_ID, item_id, customer_id, transaction_date) values (01246, 00024, 0007, '30-Sep-2024');
+insert into transactions (TRANSACTION_ID, item_id, customer_id, transaction_date) values (01247, 00025, 0014, '05-Oct-2024');
+insert into transactions (TRANSACTION_ID, item_id, customer_id, transaction_date) values (01248, 00097, 0004, '12-Oct-2024');
+insert into transactions (TRANSACTION_ID, item_id, customer_id, transaction_date) values (01249, 00025, 0001, '23-May-2024');
+insert into transactions (TRANSACTION_ID, item_id, customer_id, transaction_date) values (01250, 00002, 0002, '09-Oct-2024');
+insert into transactions (TRANSACTION_ID, item_id, customer_id, transaction_date) values (01251, 00003, 0003, '15-Nov-2024');
+insert into transactions (TRANSACTION_ID, item_id, customer_id, transaction_date) values (01252, 00099, 0004, '27-Nov-2024');
+insert into transactions (TRANSACTION_ID, item_id, customer_id, transaction_date) values (01253, 00100, 0005, '16-Nov-2024');
+
+-- Inserts for DELIVERY
+insert into delivery values (11113, 'usps', 5.99, '03-Nov-2024', 01236, 'online');
+insert into delivery values (11114, 'fed-Ex', 29.99, '03-Nov-2024', 01237, 'online');
+insert into delivery values (11115, 'ups', 5.99, '03-Nov-2024', 01238, 'physical');
+insert into delivery values (11116, 'ups', 6.99, '07-Nov-2024', 01239, 'physical');
+insert into delivery values (11117, 'ups', 7.99, '08-Nov-2024', 01240, 'physical');
+insert into delivery values (11118, 'fed-ex', 7.99, '08-Nov-2024', 01241, 'physical');
+insert into delivery values (11119, 'usps', 7.99, '11-Nov-2024', 01242, 'physical');
+insert into delivery values (11120, 'usps', 12.99, '04-May-2024', 01243, 'online');
+insert into delivery values (11121, 'fed-Ex', 12.99, '06-Aug-2024', 01244, 'online');
+insert into delivery values (11122, 'ups', 13.99, '08-Jul-2024', 01245, 'online');
+insert into delivery values (11123, 'usps', 8.99, '12-Oct-2024', 01246, 'physical');
+insert into delivery values (11124, 'fed-ex', 19.99, '21-Nov-2024', 01247, 'physical');
+insert into delivery values (11125, 'fed-ex', 18.99, '20-Nov-2024', 01248, 'physical');
+insert into delivery values (11126, 'fed-ex', 17.99, '19-Nov-2024', 01249, 'physical');
+insert into delivery(DEL_ID, del_carrier, del_cost, transaction_id, store_type) values (11127, 'usps', 5.99, 01250, 'online');
+insert into delivery(DEL_ID, del_carrier, del_cost, transaction_id, store_type) values (11128, 'usps', 5.99, 01252, 'online');
+
+
---
+++ samples/325triggers.sql
@@ -0,0 +1,21 @@
+CREATE TRIGGER item_audit
+AFTER UPDATE ON ITEM
+FOR EACH ROW
+BEGIN
+INSERT INTO ITEM_audit (column_name, old_value, new_value) VALUES ('ITEM_ID', :OLD.ITEM_ID, :NEW.ITEM_ID);
+INSERT INTO ITEM_audit (column_name, old_value, new_value) VALUES ('ITEM_TYPE', :OLD.ITEM_TYPE, :NEW.ITEM_TYPE);
+INSERT INTO ITEM_audit (column_name, old_value, new_value) VALUES ('ITEM_PRICE', :OLD.ITEM_PRICE, :NEW.ITEM_PRICE);
+END;
+
+
+CREATE TRIGGER empl_audit
+AFTER INSERT ON EMPLOYEE
+FOR EACH ROW
+BEGIN
+INSERT INTO EMPLOYEE_audit (column_name, old_value, new_value) VALUES ('EMPL_ID', :OLD.EMPL_ID, :NEW.EMPL_ID);
+INSERT INTO EMPLOYEE_audit (column_name, old_value, new_value) VALUES ('EMPL_FNAME', :OLD.EMPL_FNAME, :NEW.EMPL_FNAME);
+INSERT INTO EMPLOYEE_audit (column_name, old_value, new_value) VALUES ('EMPL_LNAME', :OLD.EMPL_LNAME, :NEW.EMPL_LNAME);
+INSERT INTO EMPLOYEE_audit (column_name, old_value, new_value) VALUES ('SALARY', :OLD.SALARY, :NEW.SALARY);
+END;
+
+
